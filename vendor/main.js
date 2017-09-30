315fd7ba94346c126196fba0b8b523b4eb1a5187
Hydrosaur <Korbin.Deary45@gmail.com>
2017-09-29 22:38:44 -0700

    Fixed Course Doubling Bug, Added Course Index, and Different Font.

--- vendor/main.js
+++ vendor/main.js
@@ -1,3 +1,89 @@
+var courses;
+var effects = [
+  "bounce",
+  "flash",
+  "pulse",
+  "rubberBand",
+  "shake",
+  "swing",
+  "tada",
+  "wobble",
+  "flip"
+];
+
+if(localStorage.getItem('courses') === null){
+  localStorage.setItem('courses', '[]');
+}
+courses = JSON.parse(localStorage.getItem('courses'));
+
+updateCourseIndex();
+
+$("#learnButton").on("click", function(e){
+  if($("#codeInput").val() !== ""){
+    $("#learnButton").attr("disabled", "");
+    loadCourses($("#codeInput").val());
+    $("#codeInput").val("");
+  }
+});
+
+function loadCourses(code) {
+  $.ajax({
+    url: codeToLink(code),
+    data: {},
+    success: function(res){
+      var xmlConvert = new X2JS();
+      var xml = new XMLSerializer().serializeToString(res);
+      var course = xmlConvert.xml_str2json(xml).rss.channel;
+      var courseName = course.title.replace("Treehouse - ", "");
+      course.code = code;
+      var saveableCourse = {
+        code: course.code,
+        title: course.title.replace("Treehouse - ", ""),
+        videos: videosToProgress(course.item)
+      };
+      var courseIndex = courses.findIndex(containsCode);
+      var wasCodeUsed = courses.some(containsCode);
+
+      if(!wasCodeUsed){
+        courses.push(saveableCourse);
+        localStorage.setItem('courses', JSON.stringify(courses));
+        $("#courseModal .modal-body img").attr("class", "center-block");
+        setTimeout(function(){
+          $("#courseModal .modal-body img").addClass("animated flip");
+        }, 1000);
+        $("#courseModalName").text(`Congrats! You got the ${courseName} course!`);
+        $("#courseModal").modal('show');
+        updateCourseIndex();
+      }
+      $("#courseName").text(courseName);
+      $("#videos").empty();
+      console.log(courses);
+      course.item.forEach(function(item, idx){
+        console.log(saveableCourse.videos[idx]);
+        $("#videos").append(`
+          <div class="col-lg-3 col-md-4 col-sm-6 portfolio-item">
+            <div class="card h-100">
+              <a href="${item.enclosure._url}" target="_blank"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+              <div class="card-body">
+                <h4 class="card-title">
+                  <a href="${item.enclosure._url}" target="_blank">${idx+1}. ${item.title}</a>
+                </h4>
+                <p class="card-text">${item.description}</p>
+                <label>Done? <label><input type="checkbox" class="doneBox" ${courses[courseIndex].videos[idx].done ? "checked=''" : ""} data-id="${idx}">
+              </div>
+            </div>
+          </div>
+        `);
+      });
+      createDoneBoxHandlers(code);
+      $("#learnButton").removeAttr("disabled");
+    },
+    error: function(){
+      $("#learnButton").removeAttr("disabled");
+    }
+  });
+}
+
 function codeToLink(string){
   var code = atob(string.replace(/#/g, "="));
   var stringCode = code.split("").reverse().join("");
@@ -8,31 +94,52 @@ function encodeLearn(string){
   return btoa(string.replace("https://teamtreehouse.com/library/", "").split("").map((item, idx, a) => {return a[(a.length-idx)-1]}).join("")).replace(/=/g, "#");
 }
 
-$("#learnButton").on("click", function(e){
-  var link = codeToLink($("#codeInput").val());
-  $.ajax({
-    url: link,
-    data: {},
-    success: function(res){
-      var xmlConvert = new X2JS();
-      var xml = new XMLSerializer().serializeToString(res);
-      var course = xmlConvert.xml_str2json(xml).rss.channel;
-      $("#courseName").text(course.title.replace("Treehouse - ", ""));
-      course.item.forEach(function(item, idx){
-        $("#videos").append(`
-          <div class="col-lg-3 col-md-4 col-sm-6 portfolio-item">
-            <div class="card h-100">
-              <a href="${item.enclosure._url}" target="_blank"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-              <div class="card-body">
-                <h4 class="card-title">
-                  <a href="${item.enclosure._url}" target="_blank">${idx+1}. ${item.title}</a>
-                </h4>
-                <p class="card-text">${item.description}</p>
-              </div>
-            </div>
-          </div>
-        `);
-      });
+function videosToProgress(videos){
+  return videos.map((a,b) => ({index: b, done: false}));
+}
+
+function calculateProgress(videos){
+  var reducer = videos.reduce((acc, val) => {
+    return (val.done ? 1:0) + acc;
+  }, 0);
+  return ((reducer / videos.length) * 100).toFixed(0) + "%";
+}
+
+function createDoneBoxHandlers(code){
+  $(".doneBox").change(function(e){
+    var index = Number($(this).data("id"));
+    var courseIndex = courses.findIndex(containsCode);
+    if($(this)[0].checked) {
+      courses[courseIndex].videos[index].done = true;
+      updateCourseIndex();
+    } else {
+      courses[courseIndex].videos[index].done = false;
+      updateCourseIndex();
     }
+    localStorage.setItem('courses', JSON.stringify(courses));
   });
-});
+}
+
+function createCourseIndexHandlers(){
+  $("#courseIndex tbody tr").click(function(e){
+    $("#codeInput").val($(this).find("td").eq(2).text());
+  });
+}
+
+function updateCourseIndex(){
+  $("#courseIndex tbody").empty();
+  courses.forEach(function(item, idx){
+    $("#courseIndex tbody").append(`
+      <tr>
+        <td>${item.title}</td>
+        <td>${calculateProgress(item.videos)}</td>
+        <td>${item.code}</td>
+      </tr>
+    `);
+  });
+  createCourseIndexHandlers();
+}
+
+function containsCode(item){
+  return (item) => {return item.code === code;}
+}
